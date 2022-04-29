dcf7d645fd372e675bf008ac1c21c3be53c6a322
Rodrigo Oliveira <rodrigofrontback@gmail.com>
2022-04-29 18:52:51 -0300

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import Home from './home/Home'
 import Nav from './nav/Nav'
+import Contatos from './contatos/Contatos';
 
 function App() {
   return (
     <div className="App">
         <Home/>
         <Nav/>
+        <Contatos/>
     </div>
   );
 }
---
+++ src/contatos/Contatos.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './contatos.css'
+
+export default function Contatos(){
+    return(
+        <div className="container divcontatos">
+            <h3 className="titlecab h1">Contatos</h3>
+           
+            <h4 className="h3 itenstitle">Telefone:</h4><p class="text-center">4656-1781</p>
+          
+            <h4 className="h3 itenstitle">Whatsapp:</h4><p class="text-center">(11) 97574-3625</p>
+           
+            <h4 className="h3 itenstitle">Email:</h4><p class="text-center">moveisespanha@gmail.com</p>
+
+        </div>
+    )
+}
