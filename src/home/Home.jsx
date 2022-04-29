8da70e59c1eb20ef45c80ba74532b74feea13e2e
Rodrigo Oliveira <rodrigofrontback@gmail.com>
2022-04-29 13:21:33 -0300

    First commit

---
+++ src/home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './home.css'
+import Espanha from '../imagens/espanha.gif'
+
+export default function Home(){
+    return(
+        <div className='container-fluid backhome'>
+            <div className='row'>
+                <div className='col-md-4 divimg'>
+                    <img src={Espanha} alt="Bandeira espanha" title='Bandeira Espanha' className='img-fluid'/>
+                </div>
+                <div className='col-md-8 divtext'>
+                    <h2 className='h1'>Sistema Administrativo Moveis Espanha</h2>
+                </div>
+                
+            </div>
+
+        </div>
+    )
+}
