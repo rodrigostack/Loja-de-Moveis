a02bf0da54abe5f4195438b5e0280a5eb0f127c3
Rodrigo Oliveira <rodrigofrontback@gmail.com>
2022-04-29 15:35:09 -0300

    Third commit

--- src/nav/Nav.jsx
+++ src/nav/Nav.jsx
@@ -6,7 +6,7 @@ export default function Nav(){
         <div className="divcontainer">
             <nav className="navbar navbar-expand-lg navbar-light navbarback">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="#">Sistema ADM</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
@@ -16,20 +16,40 @@ export default function Nav(){
                     <a className="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="#">Features</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Pricing</a>
+                    <a className="nav-link active" href="#">Contatos</a>
                     </li>
                     <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown link
+                    <a className="nav-link dropdown-toggle active" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Colaboradores
                     </a>
                     <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <li><a className="dropdown-item" href="#">Action</a></li>
-                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                        <li><a className="dropdown-item" href="#">Something else here</a></li>
+                        <li><a className="dropdown-item" href="#">Cadastrar</a></li>
+                        <li><a className="dropdown-item" href="#">Colaboradores</a></li>
+                        
                     </ul>
+                    
+                    </li>
+                    <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle active" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Clientes
+                    </a>
+                    <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a className="dropdown-item" href="#">Cadastrar</a></li>
+                        <li><a className="dropdown-item" href="#">Clientes</a></li>
+                        
+                    </ul>
+                    
+                    </li>
+                    <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle active" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Produtos
+                    </a>
+                    <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a className="dropdown-item" href="#">Cadastrar</a></li>
+                        <li><a className="dropdown-item" href="#">Produtos</a></li>
+                        
+                    </ul>
+                    
                     </li>
                 </ul>
                 </div>
